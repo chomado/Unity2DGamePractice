f784d9ad04170b5d67f8e540dcc4c47612032e68
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2017-12-25 18:45:08 +0900

    キャラが上にひたすら移動していく＆コンソールに座標表示

--- Assets/RobotScript.cs
+++ Assets/RobotScript.cs
@@ -11,6 +11,10 @@ public class RobotScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        var component = GetComponent<Transform>();
+        // フレーム切り替わるごとに上に移動
+        component.Translate(x: 0.0f, y: 0.1f, z: 0.0f);
+        // 座標表示
+        Debug.Log(component.position);
 	}
 }
