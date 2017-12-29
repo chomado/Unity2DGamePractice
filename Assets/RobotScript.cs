c39b0b94b0b34e300a2bb1f75f14a44f5524ba24
chomado (Madoka Chiyoda) <madoka.madoka@microsoft.com>
2017-12-29 13:40:42 +0900

    WIP

--- Assets/RobotScript.cs
+++ Assets/RobotScript.cs
@@ -4,17 +4,49 @@ using UnityEngine;
 
 public class RobotScript : MonoBehaviour {
 
+    Dictionary<string, Sprite> pose = new Dictionary<string, Sprite>()
+    {
+        { "Normal", null },
+        { "Right", null },
+        { "Left", null },
+        { "Both", null },
+    };
+
 	// Use this for initialization
 	void Start () {
-		
+
+		foreach (var key in new List<string>(pose.Keys))
+        {
+            var texture = (Texture2D)Resources.Load(path: "Rpbot_" + key);
+
+            pose[key] = Sprite.Create(
+                texture: texture
+                , rect: new Rect(x: 0, y: 0, width: texture.width, height: texture.height)
+                , pivot: new Vector2(x: 0.5f, y: 0.5f)
+            );
+        }
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        var component = GetComponent<Transform>();
-        // フレーム切り替わるごとに上に移動
-        component.Translate(x: 0.0f, y: 0.1f, z: 0.0f);
-        // 座標表示
-        Debug.Log(component.position);
+	void Update ()
+    {
+        var isLeftClicked = Input.GetMouseButton(button: 0);
+        var isRightClicked = Input.GetMouseButton(button: 1);
+
+        // マウスの左と右が両方押された
+        if (isLeftClicked && isRightClicked)
+        {
+            GetComponent<SpriteRenderer>().sprite = pose["Both"];
+        }
+        // マウスの左が両方押された
+        else if (isLeftClicked)
+        {
+
+        }
+        // マウスの右が押された
+        else if (isRightClicked)
+        {
+
+        }
 	}
 }
